1b2621ca2a0c01a53dc18c717b9506b315e5197f
yiso21 <isaacgursha569@gmail.com>
2024-02-07 10:38:11 +0300

    the  first function is not describe fully about leap Year

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ checkButton.addEventListener('click', function() {
 
     resultDiv.textContent = `The year ${yearInput} is${isLeapYearResult ? '' : ' not'} a leap year.`;
 });
-
 function isLeapYear(year) {
-    if (year % 4 === 0 && year % 400 === 0) {
-        return true;
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
     } else {
         return false;
     }
